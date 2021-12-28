4a8a5212b7e6702f6f47b8d3c2565320ef1c21de
Vojtěch Divoký <divocak@Vojtechs-MacBook-Air.local>
2021-12-28 12:03:00 +0100

    order by

--- scripts/app/rating/ratingGet.php
+++ scripts/app/rating/ratingGet.php
@@ -3,13 +3,13 @@ include_once "../../config.php";
 $_POST = json_decode(file_get_contents("php://input"), true);
 
 $resultArr = [];
-$sql = 'SELECT value, text, date_added FROM ratings WHERE user_id="' . $_POST["userId"] . '";';
+$sql = 'SELECT value, text, date_added FROM ratings WHERE user_id="' . $_POST["userId"] . '" ORDER BY date_added;';
 if ($result = mysqli_query($link, $sql)) {
     while ($row = mysqli_fetch_row($result)) {
         $resultArr[] = [
-            "val" => checkVal($row[0]),
-            "text" => checkVal($row[1]),
-            "date" => checkVal($row[2])
+            "val" => $row[0],
+            "text" => $row[1],
+            "date" => $row[2]
         ];
     }
     mysqli_free_result($result);
