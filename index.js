d8982748c79265b4380b7002603b24b62eeb39a3
Vojtěch Divoký <divocak@Vojtechs-MacBook-Air.local>
2021-12-29 23:01:24 +0100

    animations done, styling done, jquery done, waiting for images and gifs

---
+++ index.js
@@ -0,0 +1,29 @@
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.create(getAnimSettings("#download"));
+ScrollTrigger.create(getAnimSettings("#roadmap"));
+ScrollTrigger.create(getAnimSettings("#work"));
+ScrollTrigger.create(getAnimSettings("#credit"));
+ScrollTrigger.create(getAnimSettings(".work-0"));
+ScrollTrigger.create(getAnimSettings(".work-1"));
+
+function getAnimSettings(selector) {
+    return {
+        trigger: selector,
+        start: "top 70%",
+        end: "center 20%",
+        scrub: true,
+        onEnter: () => gsap.to(selector, {
+            opacity: 1
+        }),
+        onLeave: () => gsap.to(selector, {
+            opacity: 0
+        }),
+        onEnterBack: () => gsap.to(selector, {
+            opacity: 1
+        }),
+        onLeaveBack: () => gsap.to(selector, {
+            opacity: 0
+        })
+    };
+}
