263f8b1a4064b9ba651f2ce551d9015f828af069
Vojtěch Divoký <divokyvojta@gmail.com>
2022-01-09 12:18:18 +0100

    save usr mail and name, moved web images to web folder

--- scripts/app/checkUser.php
+++ scripts/app/checkUser.php
@@ -2,14 +2,14 @@
 include_once "../config.php";
 
 $_POST = json_decode(file_get_contents("php://input"), true);
-$sql = "SELECT status_id FROM users WHERE id='" . $_POST["userId"] . "';";
+$sql = "SELECT status_id, name, mail FROM users WHERE id='" . $_POST["userId"] . "';";
 if ($result = mysqli_query($link, $sql)) {
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_row($result)) {
             echo $row[0];
         }
     } else {
-        $sql = "INSERT INTO users (id, status_id) VALUES ('" . $_POST["userId"] . "', 1);";
+        $sql = "INSERT INTO users (id, status_id, name, mail) VALUES ('" . $_POST["userId"] . "', 1, '" . $_POST["name"] . "', '" . $_POST["mail"] . "');";
         if (!mysqli_query($link, $sql)) {
             echo "ERROR";
         } else {
