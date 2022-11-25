c4f8493255a45ea7d6efc2395180cad31c4eec0e
Vojtěch divoký <vojtech.divoky@polyweb.cz>
2022-11-25 16:33:58 +0100

    načítání itemů #92

--- scripts/app/schema.php
+++ scripts/app/schema.php
@@ -1,38 +1,33 @@
 <?php
-$json = prepareGeneral("general");
-
-function prepareGeneral($path, $pathPrefix = null)
-{
-    $file = "params/" . ($pathPrefix != null ? $pathPrefix . "/" : "") . $path . ".json";
-    echo $file;
-    echo "<br>";
-    $json = json_decode(file_get_contents($file), true);
-    foreach ($json as $key => &$val) {
-        if (!str_contains($key, "_switch")) {
-            loopOptions($val, $path == "general");
-        }
-    }
-    return $json;
-}
-
-function loopOptions(&$arr, $isGeneral)
-{
-    foreach ($arr as $key => &$val) {
-        if ($key == "values") {
-            loopValues($val, $isGeneral);
-        }
-    }
-}
-
-function loopValues(&$arr, $boolIsGeneral)
-{
-    foreach ($arr as $key => &$val) {
-        if (isset($val["key"])) {
-            $val["options"] = prepareGeneral($val["key"], isset($isGeneral) ? $val["key"] : null);
-        }
-    }
-}
+$json = loopAllOptions("general");
 
 echo "<pre>";
 echo json_encode($json);
 echo "</pre>";
+
+function loopAllOptions($path)
+{
+    $json = json_decode(file_get_contents("params/" . $path . ".json"), true);
+    foreach ($json as &$val) {
+        loopOneOption($val);
+    }
+    return $json;
+}
+
+function loopOneOption(&$arr)
+{
+    if (!isset($arr["isSwitch"]) && is_array($arr["values"])) {
+        foreach ($arr["values"] as &$val) {
+            loopMoreOption($val);
+        }
+    }
+}
+
+function loopMoreOption(&$arr)
+{
+    if(isset($arr["file"])){
+        $arr["options"] = loopAllOptions($arr["file"]);
+    }
+}
+// TODO reformat acc/* and comp/* files
+?>
